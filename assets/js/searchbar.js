4347200662a78149d44fd794fa3096fc5271fc6c
Jamel-h <40527847+Jamel-h@users.noreply.github.com>
2018-06-25 13:02:28 -0300

    Create searchbar.js

---
+++ assets/js/searchbar.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    document.getElementById('frmSearch').onsubmit = function() {
+        window.location = 'http://www.google.com/search?q=site:jamel-h.github.io/anyvid ' + document.getElementById('txtSearch').value;
+        return false;
+    }
+</script>
