d8fc00c9e115e65dcc731c2a5127f6cce9edef42
Jamel-h <40527847+Jamel-h@users.noreply.github.com>
2018-06-25 13:37:37 -0300

    Update searchbar.js

--- assets/js/searchbar.js
+++ assets/js/searchbar.js
@@ -1,6 +1,10 @@
-<script type="text/javascript">
-    document.getElementById('frmSearch').onsubmit = function() {
-        window.location = 'http://www.google.com/search?q=site:jamel-h.github.io/anyvid ' + document.getElementById('txtSearch').value;
-        return false;
-    }
+<script>
+    var a = document.getElementById('tfnewsearch');
+    a.addEventListener('submit',function(e) {
+        e.preventDefault();
+        var b = document.getElementById('tftextinput').value;
+        window.location.href = 'http://mywebsite.com/'+b;
+
+    });
+
 </script>
